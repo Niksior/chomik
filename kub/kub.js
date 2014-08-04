13601a8ad52275137e7db0d36574de22150e1a80
Niksior <heroes.VS.misio@gmail.com>
2014-08-04 20:14:59 +0200

    testy testy

---
+++ kub/kub.js
@@ -0,0 +1,48 @@
+function main()
+{
+  var WMIN = -99;
+  var WMAX =  99;
+  var N    =  80;
+  var d    = new Array(N);
+  var lw   = new Array(WMAX - WMIN + 1);
+  var i,j,t;
+
+
+
+  for(i = 0; i < N; i++)
+    d[i] = WMIN + Math.floor(Math.random() * (WMAX - WMIN + 1));
+
+
+
+  t = "Przed sortowaniem:<BR><BR>";
+  for(i = 0; i < N; i++)
+    if((i + 1) % 20) t += d[i] + " "; else t += d[i] + "<BR>";
+  t += "<BR>";
+
+// sortujemy
+
+// najpierw zerujemy liczniki
+
+  for(i = WMIN; i <= WMAX; i++) lw[i - WMIN] = 0;
+
+// zliczamy w odpowiednich licznikach wystąpienia
+// wartości elementów sortowanego zbioru
+
+  for(i = 0; i < N; i++) lw[d[i] - WMIN]++;
+
+// zapisujemy do zbioru wynikowego numery niezerowych liczników
+// tyle razy, ile wynosi ich zawartość
+
+  j = 0;
+  for(i = WMIN; i <= WMAX; i++) while(lw[i - WMIN]--) d[j++] = i;
+
+// wyświetlamy zawartość zbioru po sortowaniu
+
+  t += "Po sortowaniu:<BR><BR>";
+  for(i = 0; i < N; i++)
+    if((i + 1) % 20) t += d[i] + " "; else t += d[i] + "<BR>";
+
+// koniec
+
+  document.getElementById("t_out").innerHTML = t;
+} 

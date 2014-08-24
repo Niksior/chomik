ac1442a0ef0f611bcde37d5eb63b906e938ad5ad
Niksior <heroes.VS.misio@gmail.com>
2014-08-24 14:07:03 +0200

    canvas sztart

--- js.js
+++ js.js
@@ -1,3 +1,10 @@
 //alert("Ta strona NIE używa ciasteczek");
 //setTimeout("alert(\"Weź coś kliknij\")", 5000);
 
+try {
+	document.createElement("canvas").getContext("2d");
+	document.getElementById("support").innerHTML = "Wspiera.";
+} catch (e) {
+	document.getElementById("support").innerHTML = "Nie wspiera.";
+}
+
--- js2.js
+++ js2.js
@@ -20,3 +20,56 @@ function lvl()
 	document.getElementById("wlvl").innerHTML = "Poziom tej strony jest równy poziomowi Twojego umusłu czyli " + a;
 }
 
+function drawDiagonal() {
+	var canvas = document.getElementById('diagonal');
+	var context = canvas.getContext('2d');
+
+	context.save();
+
+	context.translate(70, 140);
+
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(70, -70);
+	context.stroke();
+
+	context.restore();
+	}
+
+function createCanopyPath(context) {
+	context.beginPath();
+	context.moveTo(-25, -60);
+	context.lineTo(-10, -90);
+	context.lineTo(-20, -90);
+	context.lineTo(-5, -120);
+	context.lineTo(-15, -120);
+	context.lineTo(0, -150);
+
+	context.lineTo(15, -120);
+	context.lineTo(5, -120);
+	context.lineTo(20, -90);
+	context.lineTo(10, -90);
+	context.lineTo(25, -60);
+
+	context.closePath();
+}
+
+window.addEventListener("load", drawDiagonal, true);
+
+function drawTrails() {
+	var canvas = document.getElementById('trails');
+	var context = canvas.getContext('2d');
+
+	context.save();
+	context.translate(130, 250);
+
+	createCanopyPath(context);
+
+	context.lineWidth = 5;
+	context.lineJoin = 'round';
+	context.strokeStyle = '#28f10d';
+	context.stroke();
+	context.restore();
+}
+
+window.addEventListener("load", drawTrails, true);

e00319e52875832a3e3c3b66bebde6181abc221e
Niksior <heroes.VS.misio@gmail.com>
2014-09-03 08:48:28 +0200

    cc

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 //alert("Ta strona NIE używa ciasteczek");
 //setTimeout("alert(\"Weź coś kliknij\")", 5000);
 
-try {
-	document.createElement("canvas").getContext("2d");
-	document.getElementById("support").innerHTML = "Wspiera.";
-} catch (e) {
-	document.getElementById("support").innerHTML = "Nie wspiera.";
+if (Modernizr.canvas) {
+  alert("gz");
+} else {
+  alert("Twoja przegląarka nie obsługuje HTML5. Zaktualizuj ją aby strona prawidłowo się wyświetlała.");
 }
 
